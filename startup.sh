89dd85b2548f7e2d40f4445099d6bbc60db04c48
slyg <syl.faucherand@gmail.com>
2015-03-27 22:58:35 +0100

    Replace shell script by docker-compose to build stack

--- startup.sh
+++ startup.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# stop existing containers
-docker stop webapp reverse_proxy
-
-# remove existing containers
-docker rm webapp reverse_proxy
-
-# setup containers with exposed ports
-docker run -d -p 9000:9000 --name webapp slyg/node-app
-docker run -d -p 80:80 --name reverse_proxy --link webapp:webapp slyg/nginx
-
-echo "Listening on http://$(boot2docker ip)"
+echo "Will be listening to http://$(boot2docker ip)"
+docker-compose up
