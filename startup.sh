1615958bc1f98688f871a8cd2358f61cd3900f33
slyg <syl.faucherand@gmail.com>
2015-03-30 14:59:34 +0200

    Automate container restarts on filechange (via g/nodemon)

--- startup.sh
+++ startup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# start containers from .yml conf
+docker-compose up -d
+
+# inform about host to use
 echo "Will be listening to http://$(boot2docker ip)"
-docker-compose up
+
+# watch for file changes to restart containers
+nodemon --watch webapp/ -x 'docker-compose restart'
+
+# Kill containers on exit
+trap 'docker-compose kill' EXIT
